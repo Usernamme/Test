ade39c94f942aae78aac1a61bf4d62fcf1df4ca7
Usernamme <daviddanis940@gmail.com>
2014-09-19 20:19:42 +0200

    Update Fibs.py

--- Fibs.py
+++ Fibs.py
@@ -1,11 +1,14 @@
+#checks if the latest fib num is under the limit
 def quit_trigger(numlimit,  a, b):
     if b >= numlimit > 0:
         return True
     else:
         return False
 
+"""returns a list of fib numbers - e.g. [1, 1, 2, 3, 5, 8, ...]
+   numlimit - highest number generated - use 0 for no numlimit 
+   limit    - number of times it runs: each run produces 2 numbers - use half of the number of numbers you want to generate"""
 def fibs(numlimit, limit): 
-#numlimit - highest number generated - use 0 for no numlimit | limit - number of times it runs: EACH RUN PRODUCES TWO NEW NUMBERS
     a = 1
     b = 1
     fibonacci = []
