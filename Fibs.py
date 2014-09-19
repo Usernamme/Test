c4aaff3476af51215e712a903fb1fab8856b79f4
Usernamme <daviddanis940@gmail.com>
2014-09-19 20:10:33 +0200

    Update Fibs.py

--- Fibs.py
+++ Fibs.py
@@ -4,12 +4,16 @@ def quit_trigger(numlimit,  a, b):
     else:
         return False
 
-def fibs(numlimit, limit): #numlimit - highest number generated - use 0 for no numlimit | limit - number of numbers generated
+def fibs(numlimit, limit): 
+#numlimit - highest number generated - use 0 for no numlimit | limit - number of times it runs: EACH RUN PRODUCES TWO NEW NUMBERS
     a = 1
     b = 1
+    fibonacci = []
+    fibonacci.extend([a,b])
     for x in range(1, limit):
         a += b
         b += a
+        fibs.extend([a,b])
         if quit_trigger(numlimit, a, b):
-            return (a, b)
-    return (a, b)
+            return fibonacci
+    return fibonacci
