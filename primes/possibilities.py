045d05f8545d032d95465c72d51e6890095a1baa
Usernamme <daviddanis940@gmail.com>
2014-09-20 08:22:52 +0200

    Create possibilities.py

---
+++ primes/possibilities.py
@@ -0,0 +1,6 @@
+possibilities = [x for x in range(8, 10**6) if x % 2 != 0 and x % 3 != 0 and x % 5 != 0 and x % 7 != 0]
+my_file = open("possibilities.txt", "r+")
+for data in possibilities:
+    my_file.write("%s\n" % str(data))
+my_file.close()
+
